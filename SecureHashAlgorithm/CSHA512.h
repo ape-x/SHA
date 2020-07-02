23231ea211b0ce9f1e2f0ee695ddbc31e973aac1
Mihnea Stefan <blackmoose@Mihneas-MacBook-Pro.local>
2020-07-02 22:22:04 +0300

    add SHA512 implementation in C, refactor SHA512 in Swift

---
+++ SecureHashAlgorithm/CSHA512.h
@@ -0,0 +1,20 @@
+//
+//  CSHA512.h
+//  SecureHashAlgorithm
+//
+//  Created by Mihnea Stefan on 02/07/2020.
+//  Copyright © 2020 Mihnea Stefan. All rights reserved.
+//
+
+#ifndef CSHA512_h
+#define CSHA512_h
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+void hashComputation(char* input);
+
+#endif /* CSHA512_h */
--- SecureHashAlgorithm/main.swift
+++ SecureHashAlgorithm/main.swift
@@ -8,61 +8,7 @@
 
 import Foundation
 
-
-@discardableResult func preprocessing(input : String)->[Int]{
-    var binary = [Int]()
-    var array = [Int]()
-    for c in input{
-        binary.append(Int(c.asciiValue!))
-    }
-    let l = binary.count*8
-    var k = 112-(((l/8)%128)+1)
-    if k<0{
-        k = k*(-1)
-        k = 128-k
-    }
-    if l<256{ // l is 8 bits long
-        array.append(l)
-        for _ in 0..<15{
-        array.append(0)
-        }
-        for _ in 0..<k{
-            array.append(0)
-        }
-        array.append(128)
-    }else{//l is longer than 8 bits
-        var buffer = l/256
-        array.append(l%256)
-        while true{
-            array.append(buffer%256)
-            buffer = buffer/256
-            if buffer<256{
-                if array.count>15{
-                    array.append(buffer+128)
-                    for _ in 0..<k{
-                        array.append(0)
-                    }
-                    break
-                }else{
-                array.append(buffer)
-                while array.count<16{
-                    array.append(0)
-                    }
-                    for _ in 0..<k{
-                        array.append(0)
-                    }
-                    array.append(128)
-                break
-                }
-            }
-        }
-    }
-    binary+=array.reversed()
-    return binary
-}
-
-var converter = Converter()
-var qq = ""
+var qq = "abc"
 
 var q = SHA1(seed: "abc")
 var t = SHA256(seed: "abc")
@@ -71,3 +17,4 @@ print(Date.init())
 r.hashComputation()
 print(r.messageDigest!)
 print(Date.init())
+
